67fc1d015fbcc224b526d6f7416281aacc0184f4
GiulianoCretella <giulianocretella.1@gmail.com>
2022-03-28 09:41:45 +0200

    correzione ancoraggio su a

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 let menu = document.querySelector('.hamburger-menu');
 console.log(menu);
 
-let burgerIcon = document.querySelector('.fa-bars');
+let burgerIcon = document.querySelector('.header-right > a');
 console.log(burgerIcon);
 
-let escIcon = document.querySelector('.fa-times');
+let escIcon = document.querySelector('.hamburger-menu > a');
 
 burgerIcon.addEventListener('click', function(){
     menu.style.display = 'block';
